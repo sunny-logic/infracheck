cd65ff3907f67848a39c03fc26d3c5ba162f1b12
root <root@ip-172-31-49-206.ec2.internal>
2020-06-04 14:21:37 +0000

    Corrected root login check

--- infracheck.sh
+++ infracheck.sh
@@ -3,4 +3,4 @@ echo "Hello World, My Name is Prish, Time is `date`" >> /tmp/infra_check_log_fil
 echo "The Mount available are : \n `cat /etc/fstab`" >> /tmp/infra_check_log_file.log
 echo "The Mount available are : \n `df -h`" >> /tmp/infra_check_log_file.log
 echo "The users available are : \n `cat /etc/passwd`" >> /tmp/infra_check_log_file.log
-echo "Root login available : `cat /etc/ssh/sshd_config | grep "PermitRootLogin`"  >> /tmp/infra_check_log_file.log
+echo "Root login available : `cat /etc/ssh/sshd_config | grep -v 'PermitRootLogin'`"  >> /tmp/infra_check_log_file.log
