9ea4e2e44cc6d23c9ba55f63278741be4c583b4f
root <root@ip-172-31-49-206.ec2.internal>
2020-06-04 12:33:39 +0000

    added few more checks

--- infracheck.sh
+++ infracheck.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 echo "Hello World, My Name is Prish, Time is `date`" >> /tmp/infra_check_log_file.log
+echo "The Mount available are : \n `cat /etc/fstab`" >> /tmp/infra_check_log_file.log
+echo "The Mount available are : \n `df -h`" >> /tmp/infra_check_log_file.log
+echo "The users available are : \n `cat /etc/passwd`" >> /tmp/infra_check_log_file.log
+
+
+
