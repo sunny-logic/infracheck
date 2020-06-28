020241ba76b23362168dc749285f2c20bd6ea79b
sunny-logic <vk.sanmati@gmail.com>
2020-06-28 23:15:34 +0100

    Create a new logfile everytime the script runs

--- infracheck.sh
+++ infracheck.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Hello World, My Name is Prish, Time is `date`" >> /tmp/infra_check_log_file.log
+echo "Hello World, My Name is Prish, Time is `date`" > /tmp/infra_check_log_file.log
 echo "The Mount available are :  `cat /etc/fstab`" >> /tmp/infra_check_log_file.log
 echo "The Mount available are :  `df -h`" >> /tmp/infra_check_log_file.log
 echo "The users available are :  `cat /etc/passwd`" >> /tmp/infra_check_log_file.log
