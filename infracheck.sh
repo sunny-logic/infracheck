8feb2c21a1d62d44e49bfc719736555c0c63d559
sunny-logic <vk.sanmati@gmail.com>
2023-04-26 10:21:56 +0100

    Update infracheck.sh

--- infracheck.sh
+++ infracheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "Hello World, My Name is Prish, Time is `date` `whoami`" > /tmp/infra_check_log_file.log
-echo "The Mount available are :  `cat /etc/fstab`" >> /tmp/infra_check_log_file.log
-echo "The Mount available are :  `df -h`" >> /tmp/infra_check_log_file.log
+echo "Hello World, I'm `whoami`, Time is `date` "    > /tmp/infra_check_log_file.log
+echo "The Mount available are :  `cat /etc/fstab`"  >> /tmp/infra_check_log_file.log
+echo "The Mount available are :  `df -h`"           >> /tmp/infra_check_log_file.log
 echo "The users available are :  `cat /etc/passwd`" >> /tmp/infra_check_log_file.log
 echo "The Etc Resolve File records available are  :  `cat /etc/resolv.conf`" >> /tmp/infra_check_log_file.log
